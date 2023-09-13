d821e2fa1f7fdf49e3aaa65dabc67335ecbeb9e5
chris <cstieg@metalcompinc.com>
2023-09-13 16:35:49 -0400

    Handle import() returning functions in lowercase

--- src/scripts/Events/OnRenderedEvent.js
+++ src/scripts/Events/OnRenderedEvent.js
@@ -1,6 +1,10 @@
 export default function onRenderedEvent(app, route) {
-    if (route.importedScript && route.importedScript.onRendered) {
-        if (typeof route.importedScript.onRendered !== "function") { throw "onRendered must be function"; }
-        route.importedScript.onRendered(route, app);
+    if (!route.importedScript) { return; }
+    if (route.importedScript.onrendered) {
+        route.importedScript.onRendered = route.importedScript.onrendered;
     }
+    if (!route.importedScript.onRendered) { return; }
+    if (typeof route.importedScript.onRendered !== "function") { throw "onRendered must be function"; }
+
+    route.importedScript.onRendered(route, app);
 }
